74c92a641266cffad5938542a4aa2273a2bca56d
Khaled Ramadan <khaled@neo.ae>
2022-08-24 20:11:25 +0300

    We explained the angular concept and some basics like binding types, components github task dicussion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cms';
+  mode = 'light';
+  cafe = 'you';
+  isVisible = false;
+  dynamicColor = 'blue'
+  divClasses = {
+    'light-mode': this.mode === 'light',
+    'dark-mode': this.mode !== 'light'
+  };
+  styles = {
+    'background-color': this.mode === 'light' ? 'yellow' : 'black',
+    'color': 'white',
+    'margin-top': '10px'
+  };
+
+  logSomething() {
+    this.mode = this.mode === 'dark' ? 'light' : 'dark';
+    this.styles['background-color'] = this.mode === 'light' ? 'yellow' : 'black';
+  };
 }
