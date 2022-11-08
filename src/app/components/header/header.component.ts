75520cf128958eb87ab88e3798d95df5adb725ed
Khaled Ramadan <khaled@neo.ae>
2022-11-08 19:51:48 +0200

    Fix: the logout style

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,14 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  imageSrc = 'src/assets/Images/Logo1.png'; 
-  
+  imageSrc = 'src/assets/Images/Logo1.png';
+
+  constructor(private router: Router) {}
+
   ngOnInit(): void {
   }
 
-  onLogout(){
-      localStorage.removeItem('token');
-      localStorage.clear();
+  onLogout() {
+    localStorage.removeItem('token');
+    this.router.navigateByUrl('/login');
   }
 
 }
